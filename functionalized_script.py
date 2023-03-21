dc7a1166483fa951a550ecc9367508108c246034
Ugur Saricam <105130977+ugursaricam@users.noreply.github.com>
2023-03-21 23:24:48 +0300

    Update functionalized_script.py

--- functionalized_script.py
+++ functionalized_script.py
@@ -27,17 +27,17 @@ def replace_with_thresholds(dataframe, variable):
 
 def retail_data_prep(dataframe):
     dataframe.dropna(inplace=True)
-    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
-    dataframe = dataframe[dataframe["Quantity"] > 0]
-    dataframe = dataframe[dataframe["Price"] > 0]
-    replace_with_thresholds(dataframe, "Quantity")
-    replace_with_thresholds(dataframe, "Price")
+    dataframe = dataframe[~dataframe['Invoice'].str.contains('C', na=False)]
+    dataframe = dataframe[dataframe['Quantity'] > 0]
+    dataframe = dataframe[dataframe['Price'] > 0]
+    replace_with_thresholds(dataframe, 'Quantity')
+    replace_with_thresholds(dataframe, 'Price')
     return dataframe
 
 
 def create_invoice_product_df(dataframe, id=False):
     if id:
-        return dataframe.groupby(['Invoice', "StockCode"])['Quantity'].sum().unstack().fillna(0). \
+        return dataframe.groupby(['Invoice', 'StockCode'])['Quantity'].sum().unstack().fillna(0). \
             applymap(lambda x: 1 if x > 0 else 0)
     else:
         return dataframe.groupby(['Invoice', 'Description'])['Quantity'].sum().unstack().fillna(0). \
@@ -45,26 +45,26 @@ def create_invoice_product_df(dataframe, id=False):
 
 
 def check_product(dataframe, StockCode):
-    return df.loc[df["StockCode"] == StockCode][["Description"]].values[0].tolist()
+    return df.loc[df['StockCode'] == StockCode][['Description']].values[0].tolist()
 
 
-def create_rules(dataframe, id=True, country = "France"):
+def create_rules(dataframe, id=True, country = 'France'):
     dataframe = dataframe[dataframe['Country'] == country]
     dataframe = create_invoice_product_df(dataframe, id)
     frequent_itemsets = apriori(dataframe, min_support=0.01, use_colnames=True)
-    rules = association_rules(frequent_itemsets, metric="support", min_threshold=0.01)
+    rules = association_rules(frequent_itemsets, metric='support', min_threshold=0.01)
     return rules
 
 df = retail_data_prep(df)
 rules = create_rules(df)
 
 def arl_recommender(rules_df, product_id, rec_count=1):
-    sorted_rules = rules_df.sort_values("lift", ascending=False)
+    sorted_rules = rules_df.sort_values('lift', ascending=False)
     recommendation_list = []
-    for i, product in enumerate(sorted_rules["antecedents"]):
+    for i, product in enumerate(sorted_rules['antecedents']):
         for j in list(product):
             if j == product_id:
-                recommendation_list.append(list(sorted_rules.iloc[i]["consequents"])[0])
+                recommendation_list.append(list(sorted_rules.iloc[i]['consequents'])[0])
 
     return recommendation_list[0:rec_count]
 
